dad5a96a8d0478d1af57c7f09c8eea3310a69083
Charlie-XIAO <yx2436@nyu.edu>
2026-04-07 21:05:29 -0400

    refactor: declare modules directly in main.rs and remove unnecessary lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,54 @@
+mod ast;
+mod codegen;
+mod error;
+mod parse;
+mod source;
+mod tokenize;
+mod types;
+mod utils;
+
+use std::path::PathBuf;
 use std::process::ExitCode;
 
+use clap::Parser as _;
+
+use crate::codegen::Codegen;
+use crate::error::Result;
+use crate::parse::Parser;
+use crate::source::Source;
+use crate::tokenize::Tokenizer;
+
+/// The Cha C compiler (chacc).
+#[derive(Debug, clap::Parser)]
+struct Cli {
+    /// The input file path, or "-" for stdin.
+    input: PathBuf,
+    /// The output file path.
+    #[arg(short, default_value = "a.out")]
+    output: PathBuf,
+}
+
+impl Cli {
+    fn run(self) -> Result<()> {
+        let source = if self.input.as_os_str() == "-" {
+            Source::from_stdin()?
+        } else {
+            Source::from_path(&self.input)?
+        };
+
+        let tokens = Tokenizer::new(&source).tokenize()?;
+        let program = Parser::new(&source, tokens).parse_program()?;
+        let codegen = Codegen::new(&source, &self.output)?;
+        codegen.generate(program)?;
+
+        Ok(())
+    }
+}
+
 fn main() -> ExitCode {
-    match chacc::run() {
+    let cli = Cli::parse();
+
+    match cli.run() {
         Ok(()) => ExitCode::SUCCESS,
         Err(e) => {
             eprintln!("{e}");
